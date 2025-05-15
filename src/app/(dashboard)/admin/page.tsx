b1788ed798df8fc2827f8ac253f94cb39812db56
atif-maxgo123 <atif.maxgo@gmail.com>
2025-05-15 12:42:57 +0500

    User Cards complete

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,22 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div>Admin Page</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="flex justify-between gap-4 flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 };
 
 export default AdminPage;
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -24,7 +24,7 @@ export default function DashboardLayout({
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
         <Navbar />
-        {children}R
+        {children}
       </div>
     </div>
   );
